2e43f4d234bcd7196535ce3fff617a1398780d1c
Munira Tabassum <munira.mt2013@gmail.com>
2023-03-30 14:40:28 -0400

    commit demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import SelectionBox from './components/selectionBox';
-import {useState, useEffect} from 'react';
-import {httpGET,httpPOST} from "./api";
+import { useState, useEffect } from 'react';
+import { httpGET, httpPOST } from "./api";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
     const postCountryURL = "https://xc-countries-api.fly.dev/api/countries/";
@@ -23,16 +25,16 @@ function App() {
     useEffect(() => {
         httpGET(postCountryURL)
             .then(data => {
-                    setState({
-                        ...localState,
-                        countries: data
-                    });
-                }
+                setState({
+                    ...localState,
+                    countries: data
+                });
+            }
             );
     }, []);
 
     function handleSubmitCountry() {
-        const {newCountry, newCountryCode} = localState;
+        const { newCountry, newCountryCode } = localState;
         const payload = {
             name: newCountry,
             code: newCountryCode
@@ -47,11 +49,11 @@ function App() {
                     newCountryCode: ''
                 });
             }
-        );
+            );
     }
-    
+
     function handleSubmitState() {
-        const {newState, newStateCode, countryIdForNewState} = localState;
+        const { newState, newStateCode, countryIdForNewState } = localState;
         const payload = {
             code: newStateCode,
             name: newState,
@@ -67,20 +69,20 @@ function App() {
                     newCountryCode: ''
                 });
             }
-        );
+            );
     }
     function handleCountryChange(value) {
         let apiURL = postCountryURL + value.target.value + '/states/';
 
         httpGET(apiURL)
             .then(states => {
-                    setState({
-                        ...localState,
-                        selectedCountry: value.target.value,
-                        states: states
-                    });
-                }
-            ); 
+                setState({
+                    ...localState,
+                    selectedCountry: value.target.value,
+                    states: states
+                });
+            }
+            );
     }
 
     function handleCountryChangeForAddingState(value) {
@@ -91,51 +93,60 @@ function App() {
     }
 
     return (
-        <div>
-            <div>
-                <SelectionBox
-                    title="Country"
-                    options={localState.countries}
-                    onChangeSelection={handleCountryChange}
-                />
-                <SelectionBox
-                    title="State"
-                    options={localState.states}
-                    // onChangeSelection={handleCountryChange}
-                />
-            </div>
 
-            <div>
-                <h2>Add a new country</h2>
-                <div>
-                    <label>Country Name: </label>
-                    <input type={"text"} value={localState.newCountry} onChange={e => setState({...localState, newCountry: e.target.value})}/>
-                </div>
-                <div>
-                    <label>Country Code: </label>
-                    <input type={"text"} value={localState.newCountryCode} onChange={e => setState({...localState, newCountryCode: e.target.value})}/>
-                </div>
-                <button onClick={handleSubmitCountry}>Add Country</button>
-            </div>
-            <div>
-                <h2>Add a new state</h2>
-                <div>
+        <>
+            <div class="container" >
+                <div class="col-sm-4">
                     <SelectionBox
-                    title="Country"
-                    options={localState.countries}
-                    onChangeSelection={handleCountryChangeForAddingState}
-                    forAddingState={true}
-                />
-                    <label>State Name: </label>
-                    <input type={"text"} value={localState.newState} onChange={e => setState({...localState, newState: e.target.value})}/>
-                    <label>State Code: </label>
-                    <input type={"text"} value={localState.newStateCode} onChange={e => setState({...localState, newStateCode: e.target.value})}/>
-                    <div>
-                        <button onClick={handleSubmitState}>Add State</button>
+                        title="Country"
+                        options={localState.countries}
+                        onChangeSelection={handleCountryChange}
+                    />
+                    <SelectionBox
+                        title="State"
+                        options={localState.states}
+                    // onChangeSelection={handleCountryChange}
+                    />
+                </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+
+                            <h2 className=''>Add a new country</h2>
+                            <div>
+                                <label>Country Name: </label>
+                                <input type={"text"} value={localState.newCountry} onChange={e => setState({ ...localState, newCountry: e.target.value })} />
+                            </div>
+                            <div>
+                                <label>Country Code: </label>
+                                <input type={"text"} value={localState.newCountryCode} onChange={e => setState({ ...localState, newCountryCode: e.target.value })} />
+                            </div>
+                            <button type='button' className='btn btn-success' onClick={handleSubmitCountry}>Add Country</button>
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h2>Add a new state</h2>
+                        <div>
+                            <SelectionBox
+                                title="Country"
+                                options={localState.countries}
+                                onChangeSelection={handleCountryChangeForAddingState}
+                                forAddingState={true}
+                            />
+                            <label>State Name: </label>
+                            <input type={"text"} value={localState.newState} onChange={e => setState({ ...localState, newState: e.target.value })} />
+                            <label>State Code: </label>
+                            <input type={"text"} value={localState.newStateCode} onChange={e => setState({ ...localState, newStateCode: e.target.value })} />
+                            <div>
+                                <button type='button' className='btn btn-success' onClick={handleSubmitState}>Add State</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
             </div>
-        </div>
+
+        </>
     );
 }
 
